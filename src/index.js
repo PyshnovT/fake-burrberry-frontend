eb558d8a54dff7d4c9942efa129cfbbe7033650d
Timothy Pyshnov <lightwight99@gmail.com>
2017-08-12 19:04:43 +0300

    fix: ru locale

--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'normalize.css';
-import 'flexboxgrid2';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import { IntlProvider, addLocaleData } from 'react-intl';
-import ruLocaleData from 'react-intl/locale-data/ru';
+import React from "react";
+import ReactDOM from "react-dom";
+import "normalize.css";
+import "flexboxgrid2";
+import "./index.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+import { IntlProvider, addLocaleData } from "react-intl";
+import ruLocaleData from "react-intl/locale-data/ru";
+addLocaleData(ruLocaleData);
 
 ReactDOM.render(
   <IntlProvider locale="ru">
     <App />
   </IntlProvider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 registerServiceWorker();
