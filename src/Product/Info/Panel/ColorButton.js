c213aa8fc33e0b04210a912cb235e35edd4161ff
Timothy Pyshnov <lightwight99@gmail.com>
2017-08-12 16:55:38 +0300

    feat: colorPicker

--- src/Product/Info/Panel/ColorButton.js
+++ src/Product/Info/Panel/ColorButton.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import styled from 'styled-components';
+import React, { Component } from "react";
+import styled, { css } from "styled-components";
 
-const ColorButton = styled.button`
+const Button = styled.button`
   box-sizing: border-box;
   margin-right: 1rem;
   padding: 1.25rem;
   width: 0;
   height: 0;
-  box-shadow: inset 0px 0px 0px 1px #232122;
   border: none;
   border-radius: 1.25rem;
-  background-color: ${ props => props.value };
+  background-color: ${props => props.value};
+
+  ${props =>
+    props.active &&
+    css`
+      box-shadow: inset 0px 0px 0px 1px #232122;
+  `};
 `;
 
-const Text = styled.span`
-  display: none;
-`;
+const Text = styled.span`display: none;`;
 
-export default function(props) {
-  return (
-    <ColorButton value={props.value} active={props.active} type="button">
-      <Text>Change color to black</Text>
-    </ColorButton>
-  );
+class ColorButton extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <Button
+        value={this.props.value}
+        active={this.props.active}
+        onClick={() => this.props.onClick(this.props.index)}
+        type="button"
+      >
+        <Text>
+          Change color to {this.props.name}
+        </Text>
+      </Button>
+    );
+  }
 }
+
+export default ColorButton;
--- src/Product/Info/Panel/ColorPicker.js
+++ src/Product/Info/Panel/ColorPicker.js
@@ -1,8 +1,8 @@
-import React from "react";
+import React, { Component } from "react";
 import styled, { css } from "styled-components";
 import ColorButton from "./ColorButton";
 
-const ColorPicker = styled.div`
+const Wrapper = styled.div`
   @media only screen and (min-width: 62rem) {
     margin-bottom: 1.5rem;
   }
@@ -22,16 +22,46 @@ const ColorButtons = styled.div`
   margin-top: 1rem;
 `;
 
-export default function() {
-  return (
-    <ColorPicker>
-      <CurrentColor>
-        Colour: <Bold>Black</Bold>
-      </CurrentColor>
-      <ColorButtons>
-        <ColorButton value="#232122" />
-        <ColorButton value="#cfa880" />
-      </ColorButtons>
-    </ColorPicker>
-  );
+class ColorPicker extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { currentID: 0 };
+    this.toggle = this.toggle.bind(this);
+  }
+
+  toggle(index) {
+    this.setState({
+      currentID: index
+    });
+  }
+
+  render() {
+    const colorValues = ["#232122", "#cfa880"];
+    const colorNames = ["Black", "Honey"];
+
+    const buttonsList = colorValues.map((value, index) => {
+      return (
+        <ColorButton
+          value={value}
+          active={index == this.state.currentID}
+          index={index}
+          name={colorNames[index]}
+          onClick={this.toggle}
+        />
+      );
+    });
+
+    return (
+      <Wrapper>
+        <CurrentColor>
+          Colour: <Bold>{colorNames[this.state.currentID]}</Bold>
+        </CurrentColor>
+        <ColorButtons>
+          {buttonsList}
+        </ColorButtons>
+      </Wrapper>
+    );
+  }
 }
+
+export default ColorPicker;
