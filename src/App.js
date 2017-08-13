f948b8379833530ebd10b5919025914c9fd00511
Timothy Pyshnov <lightwight99@gmail.com>
2017-08-13 15:52:05 +0300

    fix: index.js proviers moved

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
-import React from 'react';
-import { Helmet } from 'react-helmet';
-import Header from './Header';
-import Product from './Product';
-import Footer from './Footer';
+import React from "react";
+import { IntlProvider, addLocaleData } from "react-intl";
+import ruLocaleData from "react-intl/locale-data/ru";
+import { Helmet } from "react-helmet";
+import Header from "./Header";
+import Product from "./Product";
+import Footer from "./Footer";
+
+addLocaleData(ruLocaleData);
 
 function App() {
   return (
-    <div>
-      <Helmet>
-        <title>Burberry</title>
-      </Helmet>
-      <Header />
-      <Product />
-      <Footer />
-    </div>
+    <IntlProvider locale="ru">
+      <div>
+        <Helmet>
+          <title>Burberry</title>
+        </Helmet>
+        <Header />
+        <Product />
+        <Footer />
+      </div>
+    </IntlProvider>
   );
 }
 
